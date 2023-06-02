a937fce6560d1347572ae49387b0c5bf0aa0b483
marcogiabbani <mtgiabbani@gmail.com>
2023-06-02 16:00:41 -0300

    adds just enagh components WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import React,{ useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './styles/App.css'
+import LogIn from './components/LogIn'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+
+      <div className="container row">
+        <div className="hero__img">
+
+          <h1>Hello, world!</h1>
+          <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam
+          </p>
+        </div>
+        <div className='log-in'>
+          <LogIn />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
---
+++ src/components/LogIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/LogIn.css'
+
+
+const LogIn = () => {
+  return (
+    <div>
+      <div className="">
+        <h1>Login</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default LogIn
